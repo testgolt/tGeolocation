fe014c163928e2a8cc72a74e64d3c962909c0a3b
Igor <angel@78-60-85-84.static.zebra.lt>
2015-01-26 00:14:23 +0200

    Initial Commit

---
+++ tGeolocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tGeolocation
+//
+//  Created by Igor on 26/01/15.
+//  Copyright (c) 2015 GoltApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tGeolocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tGeolocation
+//
+//  Created by Igor on 26/01/15.
+//  Copyright (c) 2015 GoltApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
